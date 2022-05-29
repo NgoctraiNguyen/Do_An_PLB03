d33f7ce09d679a90ac0b5ac185ce1fb92eb590d3
NGUYEN CHIEM GIANG <nguyenchiemgiang@gmail.com>
2022-05-29 10:06:18 +0700

    capnhatsan

---
+++ BUS/BUSKhachHang.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Do_An_PLB03.DTO;
+using Do_An_PLB03.DAL;
+
+namespace Do_An_PLB03.BUS
+{
+    internal class BUSKhachHang
+    {
+        public static string ten;
+        public static string SDTkhachhang;
+        public static void KhachHang(DTOKhachHang khachhang)
+        {
+            DALKhachHang.KhachHang(khachhang);
+        }
+        public static void getkhachhang(DTOKhachHang khachhang,string SDT)
+        {
+            DALKhachHang.getkhachhang(khachhang,SDT);
+            ten = DALKhachHang.ten;
+            SDTkhachhang = DALKhachHang.SDTkhachhang;
+        }
+    }
+}
--- DTO/DTOKhachHang.cs
+++ DTO/DTOKhachHang.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace Do_An_PLB03.DTO
 {
-	public Class1()
-	{
-	}
+    internal class DTOKhachHang
+    {
+        private int _MaKhachHang;
+        private string _TenKhachHang;
+        private string _SDTKhachHang;
+
+        public DTOKhachHang() { }
+        public DTOKhachHang(int MaKhachHang, string TenKhachHang, string SDTKhachHang)
+        {
+            this.MaKhachHang = MaKhachHang;
+            this.TenKhachHang = TenKhachHang;
+            this.SDTKhachHang = SDTKhachHang;
+
+        }
+
+        public int MaKhachHang { get => _MaKhachHang; set => _MaKhachHang = value; }
+        public string TenKhachHang { get => _TenKhachHang; set => _TenKhachHang = value; }
+        public string SDTKhachHang { get => _SDTKhachHang; set => _SDTKhachHang = value; }
+    }
 }
